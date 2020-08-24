90d44c361eccde57aca6f47f3f56895272e23eb4
Jed Fox <git@jedfox.com>
2020-08-24 15:32:01 -0400

    Factor New Yorker article parsing out to a separate file

--- api/daily-shouts.js
+++ api/daily-shouts.js
@@ -1,50 +1,13 @@
-const parseDate = require("date-fns/parse");
-const startOfToday = require("date-fns/startOfToday");
-const isValid = require("date-fns/isValid");
-const zonedTimeToUtc = require("date-fns-tz/zonedTimeToUtc");
-const { map, scrape, sendFeed } = require("../src/util");
-
-function parsePublishDate(date) {
-  const dayDate = parseDate(date, "LLLL d, y", startOfToday());
-  return zonedTimeToUtc(
-    isValid(dayDate)
-      ? dayDate
-      : parseDate(date.toLowerCase(), "h:mm aaaa", startOfToday()),
-    "America/New_York"
-  );
-}
+const { sendFeed } = require("../src/util");
+const { parseArticles } = require("../src/new-yorker");
 
 const url = "https://www.newyorker.com/humor/daily-shouts";
-const parseURL = (relURL) => new URL(relURL, url);
 module.exports = async (req, res) => {
-  const $ = await scrape(url);
-  const articles = $("[class*=itemContent i]");
-
   sendFeed(res, {
     title: "Daily Shouts",
     home_page_url: url,
     feed_url: req.url,
     favicon: "https://www.newyorker.com/favicon.ico",
-    items: map(articles, (article) => {
-      const title = article.find("h4");
-      const articleURL = parseURL(title.parents("a").attr("href"));
-      return {
-        id: articleURL,
-        url: articleURL,
-        title: title.text(),
-        summary: article.find("h5").text(),
-        image: article
-          .find("img")
-          .attr("src")
-          .replace("4:3/w_116,c_limit/", ""),
-        date_published: parsePublishDate(
-          article.find("[class*=publishDate i]").text()
-        ),
-        authors: map(article.find("[class^=byline i] a"), (link) => ({
-          name: link.text(),
-          url: parseURL(link.attr("href")),
-        })),
-      };
-    }),
+    items: await parseArticles(url),
   });
 };
