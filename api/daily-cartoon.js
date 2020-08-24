34a057d46ff7fd798cf99badd5e2de42902d19c9
Jed Fox <git@jedfox.com>
2020-08-24 15:32:13 -0400

    Add a New Yorker Daily Cartoon feed

---
+++ api/daily-cartoon.js
@@ -0,0 +1,19 @@
+const { sendFeed } = require("../src/util");
+const { parseArticles } = require("../src/new-yorker");
+
+const url = "https://www.newyorker.com/cartoons/daily-cartoon";
+module.exports = async (req, res) => {
+  sendFeed(res, {
+    title: "Daily Shouts",
+    home_page_url: url,
+    feed_url: req.url,
+    favicon: "https://www.newyorker.com/favicon.ico",
+    items: (await parseArticles(url)).map(({ image, summary, ...item }) => ({
+      ...item,
+      image,
+      content_html: `<img src="${image}">${
+        summary[0] === "“" ? `<p align="center">${summary}</p>` : ""
+      }`,
+    })),
+  });
+};
