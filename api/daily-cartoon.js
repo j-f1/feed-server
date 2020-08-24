0594e778ec8f819365b90515762595c5855edb5c
Jed Fox <git@jedfox.com>
2020-08-24 15:34:31 -0400

    An important part of the feed is its title

--- api/daily-cartoon.js
+++ api/daily-cartoon.js
@@ -4,7 +4,7 @@ const { parseArticles } = require("../src/new-yorker");
 const url = "https://www.newyorker.com/cartoons/daily-cartoon";
 module.exports = async (req, res) => {
   sendFeed(res, {
-    title: "Daily Shouts",
+    title: "Daily Cartoon",
     home_page_url: url,
     feed_url: req.url,
     favicon: "https://www.newyorker.com/favicon.ico",
