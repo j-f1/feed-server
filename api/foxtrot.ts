17b79ba0bd604d14667288c69d35395f9766bfdf
Jed Fox <git@jedfox.com>
2021-06-18 12:59:51 -0400

    Fix foxtrot

--- api/foxtrot.ts
+++ api/foxtrot.ts
@@ -15,13 +15,22 @@ export default createFeed({
     "https://1a3k5t1s1nlq3nug3z23q9ed-wpengine.netdna-ssl.com/wp-content/uploads/2020/05/cropped-favicon-quincy-1-180x180.png",
   author: { name: "Bill Amend", url },
   items: scrapeItems(url, { selector: ".entry" }, (comic) => {
-    const srcset = comic.find("img").attr("srcset")!;
+    var src: string;
+    const srcset = comic.find("img").attr("srcset");
+    if (srcset) {
+      src = parseSrcset(srcset).sort((a, b) => b.width! - a.width!)[0].url;
+    } else {
+      src = comic
+        .find("img")
+        .attr("src")!
+        .replace(/-\d+x\d+\./, ".");
+    }
     return {
       id: comic.find(".entry-title a").attr("href")!,
       author: { name: "Bill Amend", url },
       title: comic.find(".entry-title").text(),
-      content_html: `<img srcset="${srcset}">`,
-      image: parseSrcset(srcset).sort((a, b) => b.width! - a.width!)[0].url,
+      content_html: `<img ${srcset ? `srcset="${srcset}"` : `src="${src}"`}>`,
+      image: src,
       date_published: parseDate(
         comic.find(".entry-summary").text().trim(),
         "MMMM do, yyyy",
